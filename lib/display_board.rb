9ba5deed596f97d34175c222822f357e0c4c889a
Leandran M <websitesa95@gmail.com>
2021-02-26 23:00:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,8 +5,8 @@ def display_board(board)
   puts "-----------"
   puts "   | #{board[4]} |   "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = [" O "," "," "," "," X "," "," "," "," "]
+board = [" O "," "," "," "," X "," "," O "," O "," O "]
 display_board(board)
