65c5200685dfec085341bc9141815bd8f8c862b1
Leandran M <websitesa95@gmail.com>
2021-02-26 23:03:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,3 +10,14 @@ end
 
 board = [" O "," "," "," "," X "," "," O "," O "," O "]
 display_board(board)
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+board = [" X "," X "," X "," "," X "," "," O "," O "," O "]
+display_board(board)
